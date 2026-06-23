29e0b7a031b7ad4fb7f8c4a15271131eb90c252b
Nicolas123 <nicolasstevnmartinez141@gmail.com>
2026-06-23 13:57:34 -0500

    Commit 1: El Organo Vital - Creado cloudEngine

--- src/utils/cloudEngine.js
+++ src/utils/cloudEngine.js
@@ -1,4 +1,8 @@
-export async function syncData() {
+export const syncData = async () => {
+  try {
     const response = await fetch('https://olokcmfocijqlsfueyuu.supabase.co');
     return await response.json();
-}
+  } catch (error) {
+    return null;
+  }
+};
